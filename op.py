482eac2d5bf785edef31359530dbf34407564db4
SrinisaSubramani <nisasubramani@gmail.com>
2024-12-24 14:19:35 +0530

    my new op

---
+++ op.py
@@ -0,0 +1,26 @@
+a="nisa"
+print(type(a)) 
+b=1+2+8+8 
+print(type(b))
+c="bharathi"
+print(type("1,bharathi,12.3")) 
+d= None
+print(type(d)) 
+e = ("1", "dairymilk", "cherry")
+print(type(e))
+f=True
+print(type(f))
+g=False,"tharani"
+print(type(g))
+h="Ture",12.3,21,"none" 
+print(type(h)) 
+i="my new task"
+print(type(i))
+k="dataype","25",2024,"python" 
+print(type(k))
+n=False
+print(3+7<6>=9**3 and 3>1)
+m=True
+print(m+2/5<3==3)
+x=2+3
+print(9<8==9*1 or 5<3)
